7624ee957253eb567c69184f99fe48af539fd999
conglip01 <conglip01@gmail.com>
2024-07-29 02:42:11 +0700

    fix upload excel string phonenumber

--- app/Imports/ContactsImport.php
+++ app/Imports/ContactsImport.php
@@ -17,7 +17,14 @@ class ContactsImport implements ToCollection, WithHeadingRow
 
     public function collection(Collection $rows)
     {
-        $this->data = $rows;
+        $this->data = $rows->map(function ($row) {
+            return [
+                'name' => (string)$row['name'],
+                'address' => (string)$row['address'],
+                'phone_number' => (string)$row['phone_number'],
+                'group_name' => (string)$row['group_name'],
+            ];
+        });
     }
 
     public function getData()
